dab714c78ffd0966cdea7bf0f9d55d4d213893f8
alexilie <sorin.alex.ilie@gmail.com>
2022-10-05 22:11:13 -0400

    second commit

---
+++ src/main/java/com/jomo/trackzilla/jomotrackzillaapp/JomoTrackzillaAppApplication.java
@@ -0,0 +1,13 @@
+package com.jomo.trackzilla.jomotrackzillaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JomoTrackzillaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JomoTrackzillaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jomo/trackzilla/jomotrackzillaapp/JomoTrackzillaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jomo.trackzilla.jomotrackzillaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JomoTrackzillaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
